0aec62881a24385bc49520500da3634e16dd483b
Clara <anaclara16.gimenes@gmail.com>
2025-09-15 09:12:57 -0300

    Add files via upload

---
+++ Modulo 1/Exercicios/Atividade.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite o numero para atubuada: "))
+i = int(input("Digite onde a tabuada vai começar: "))
+fim = int(input("Digite até qual multiblicador vai: "))
+
+
+while i <= fim:
+    print(f"{i} x {numero} = {i * numero}")
+    i += 1                                                          
